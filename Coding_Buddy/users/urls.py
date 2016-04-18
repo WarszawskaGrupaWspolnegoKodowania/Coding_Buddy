85b7833241bcefc834079b75f75c55a51e8722ab
Aneta <anetka.wlodarczyk@gmail.com>
2016-04-18 12:42:14 +0200

    Correct name of url tags to match urls names

--- Coding_Buddy/users/urls.py
+++ Coding_Buddy/users/urls.py
@@ -10,27 +10,27 @@ urlpatterns = [
     url(
         regex=r'^$',
         view=views.UserListView.as_view(),
-        name='list'
+        name='user_list'
     ),
 
     # URL pattern for the UserRedirectView
     url(
         regex=r'^~redirect/$',
         view=views.UserRedirectView.as_view(),
-        name='redirect'
+        name='user_redirect'
     ),
 
     # URL pattern for the UserDetailView
     url(
         regex=r'^(?P<username>[\w.@+-]+)/$',
         view=views.UserDetailView.as_view(),
-        name='detail'
+        name='user_detail'
     ),
 
     # URL pattern for the UserUpdateView
     url(
         regex=r'^~update/$',
         view=views.UserUpdateView.as_view(),
-        name='update'
+        name='user_update'
     ),
 ]
