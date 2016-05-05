e7e4029c355d97ed323ca91ebc37d93fb1f14339
sylvan1 <spaczusk@gmail.com>
2016-05-05 19:36:22 +0200

    Added Skill's model.

--- Coding_Buddy/users/models.py
+++ Coding_Buddy/users/models.py
@@ -3,9 +3,14 @@ from __future__ import unicode_literals, absolute_import
 
 from django.contrib.auth.models import AbstractUser
 from django.core.urlresolvers import reverse
+from django.core.validators import MaxValueValidator
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible, force_bytes
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
+
+# from taggit.managers import TaggableManager
+from tagging.fields import TagField
+from tagging_autocomplete.models import TagAutocompleteField
 
 
 @python_2_unicode_compatible
@@ -13,14 +18,13 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    my_technology = models.CharField(_("my technology"), null=True, blank=True, max_length=1000)
     about_me = models.TextField(_("about me"), null=True, blank=True)
     avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
     my_project_experience = models.URLField(_("URL for project"), null=True, blank=True)
     phone = models.CharField(_("phone"), max_length=50, null=True, blank=True)
 
     def __str__(self):
-        return force_bytes('%s %s'  % (self.username, self.about_me))
+        return '%s %s' % (self.username, self.about_me)
 
     def get_absolute_url(self):
         return reverse('users:detail', kwargs={'username': self.username})
@@ -29,3 +33,27 @@ class User(AbstractUser):
         verbose_name = 'user'
         verbose_name_plural = 'users'
         ordering = ['username', ]
+
+
+class Skill(models.Model):
+
+    programming_lang = models.CharField(_("language"), max_length=255)
+    skills = models.ManyToManyField(User, through='SkillUser')
+
+    def __str__(self):
+        return self.programming_lang
+
+    class Meta:
+        verbose_name = 'skill'
+        verbose_name_plural = 'skills'
+
+
+class SkillUser(models.Model):
+
+    user = models.ForeignKey(User)
+    skill = models.ForeignKey(Skill)
+    level = models.PositiveSmallIntegerField(_("level"), validators=[MaxValueValidator(5)])
+
+    class Meta:
+        verbose_name = 'skill_user'
+        verbose_name_plural = 'skills_users'
